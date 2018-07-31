66378f5810046136de92894f53047e12eec1df33
carles <carles.sanz.contractor@dev.bbva.com>
2018-07-31 12:17:41 +0200

    integrate musicians service in the list

--- src/views/musicians/components/MusiciansList.vue
+++ src/views/musicians/components/MusiciansList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="musicians-list">
-    <ListCard v-for="(musician, key, index) in musicians" :key="key" :info="musician"/>
+    <ListCard v-for="(musician, key, index) in musicians" :key="key" :info="musicianListAssembler(musician)"/>
   </div>
 </template>
 
@@ -19,10 +19,33 @@ export default {
     ListCard,
   },
   created() {
-    this.musicians = this.musiciansList
+    this.musicians = this.musiciansList;
+    console.log(this.musicians);
   },
   data: () => ({
     musicians: [],
   }),
+  methods: {
+    musicianListAssembler(musician) {
+    return {
+      title: `${musician.name} ${musician['first-surname']} ${musician['second-surname']}`,
+      subtitle: '',
+      description: this.instrumentsToString(musician.instrument),
+    }
+    },
+    instrumentsToString(instruments) {
+      let instrumentsAsString = '';
+
+      instruments.forEach((instrument, index) => {
+        if (index === 0) {
+          instrumentsAsString += `${instrument.id}`;
+          } else {
+            instrumentsAsString += ` ${instrument.id}`;
+        }
+      });
+
+      return instrumentsAsString;
+    }
+  }
 };
 </script>
